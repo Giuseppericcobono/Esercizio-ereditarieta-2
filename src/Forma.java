506a7d0e77c03d89e2e64fa5bd3b21b2f559c1e9
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-04-10 15:02:20 +0200

    First Commit

---
+++ src/Forma.java
@@ -0,0 +1,6 @@
+public class Forma {
+    public double calcolaArea(){
+        System.out.print("Area della forma ");
+        return 0.0;
+    }
+}
---
+++ src/Rettangolo.java
@@ -0,0 +1,33 @@
+public class Rettangolo extends Forma{
+
+    public double base = 0.0;
+    public double height = 0.0;
+
+    public Rettangolo (double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double calcolaArea() {
+        super.calcolaArea();
+        System.out.println("Rettangolo");
+        return base * height;
+    }
+}
